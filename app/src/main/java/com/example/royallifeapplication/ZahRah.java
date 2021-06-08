4a42e7883b63999bbc1a4f91b367988d43507ad7
deathcoi <quocphong20022000@gmail.com>
2021-06-09 00:39:11 +0700

    demo

---
+++ app/src/main/java/com/example/royallifeapplication/ZahRah.java
@@ -0,0 +1,186 @@
+package com.example.royallifeapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+public class ZahRah extends AppCompatActivity {
+    ImageView imgback,bad1,bad2,bad3,bad4,bad5,bad6,bad7,bad8,bad9;
+    private String fullScreenInd;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zah_rah_more);
+        imgback = findViewById(R.id.backtoMenu5);
+        bad1 = findViewById(R.id.zahrah1);
+        bad2 = findViewById(R.id.zahrah2);
+        bad3 = findViewById(R.id.zahrah3);
+        bad4 = findViewById(R.id.zahrah4);
+        bad5 = findViewById(R.id.zahrah5);
+        bad6 = findViewById(R.id.zahrah6);
+        bad7 = findViewById(R.id.zahrah7);
+        bad8 = findViewById(R.id.zahrah8);
+        bad9 = findViewById(R.id.zahrah9);
+
+        //backmenu
+        imgback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+        fullScreenInd = getIntent().getStringExtra("fullScreenIndicator");
+
+        if ("y".equals(fullScreenInd)) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            getSupportActionBar().hide();
+
+            bad1.getLayoutParams().height = ViewGroup.LayoutParams.MATCH_PARENT;
+            bad1.getLayoutParams().width = ViewGroup.LayoutParams.MATCH_PARENT;
+            bad1.setAdjustViewBounds(false);
+            bad1.setScaleType(ImageView.ScaleType.FIT_XY);
+        }
+        bad1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(ZahRah.this,
+                        ZahRah.class);
+
+                if("y".equals(fullScreenInd)){
+                    intent.putExtra("fullScreenIndicator", "");
+                }else{
+                    intent.putExtra("fullScreenIndicator", "y");
+                }
+                ZahRah.this.startActivity(intent);
+            }
+        });
+        bad2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(ZahRah.this,
+                        ZahRah.class);
+
+                if("y".equals(fullScreenInd)){
+                    intent.putExtra("fullScreenIndicator", "");
+                }else{
+                    intent.putExtra("fullScreenIndicator", "y");
+                }
+                ZahRah.this.startActivity(intent);
+            }
+        });
+        bad3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(ZahRah.this,
+                        ZahRah.class);
+
+                if("y".equals(fullScreenInd)){
+                    intent.putExtra("fullScreenIndicator", "");
+                }else{
+                    intent.putExtra("fullScreenIndicator", "y");
+                }
+                ZahRah.this.startActivity(intent);
+            }
+        });
+        bad4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(ZahRah.this,
+                        ZahRah.class);
+
+                if("y".equals(fullScreenInd)){
+                    intent.putExtra("fullScreenIndicator", "");
+                }else{
+                    intent.putExtra("fullScreenIndicator", "y");
+                }
+                ZahRah.this.startActivity(intent);
+            }
+        });
+        bad5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(ZahRah.this,
+                        ZahRah.class);
+
+                if("y".equals(fullScreenInd)){
+                    intent.putExtra("fullScreenIndicator", "");
+                }else{
+                    intent.putExtra("fullScreenIndicator", "y");
+                }
+                ZahRah.this.startActivity(intent);
+            }
+        });
+        bad6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(ZahRah.this,
+                        ZahRah.class);
+
+                if("y".equals(fullScreenInd)){
+                    intent.putExtra("fullScreenIndicator", "");
+                }else{
+                    intent.putExtra("fullScreenIndicator", "y");
+                }
+                ZahRah.this.startActivity(intent);
+            }
+        });
+        bad7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(ZahRah.this,
+                        ZahRah.class);
+
+                if("y".equals(fullScreenInd)){
+                    intent.putExtra("fullScreenIndicator", "");
+                }else{
+                    intent.putExtra("fullScreenIndicator", "y");
+                }
+                ZahRah.this.startActivity(intent);
+            }
+        });
+        bad8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(ZahRah.this,
+                        ZahRah.class);
+
+                if("y".equals(fullScreenInd)){
+                    intent.putExtra("fullScreenIndicator", "");
+                }else{
+                    intent.putExtra("fullScreenIndicator", "y");
+                }
+                ZahRah.this.startActivity(intent);
+            }
+        });
+        bad9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(ZahRah.this,
+                        ZahRah.class);
+
+                if("y".equals(fullScreenInd)){
+                    intent.putExtra("fullScreenIndicator", "");
+                }else{
+                    intent.putExtra("fullScreenIndicator", "y");
+                }
+                ZahRah.this.startActivity(intent);
+            }
+        });
+
+    }
+}
